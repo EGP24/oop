710fb09137f779f46346e3831706551b6115659e
v.pereverza <v.pereverza@cian.ru>
2023-12-25 01:03:45 +0300

    add chrono::duration for cppLR6

--- cppLR6/main.cpp
+++ cppLR6/main.cpp
@@ -3,11 +3,14 @@
 #include "NoteTest.h"
 #include "NotebookTest.h"
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
+    auto start = chrono::steady_clock::now();
+
     testNote();
     testNotebook();
 
@@ -124,4 +127,8 @@ int main()
             }
         }
     } while (choice != 15);
+
+    auto end = chrono::steady_clock::now();
+    cout << "Затраченное время: ";
+    cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " миллисекунд" << endl;
 }
